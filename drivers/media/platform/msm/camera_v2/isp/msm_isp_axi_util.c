1bc2e6a983747601ba9a2d1a42588a1cdfbe34dd
0ctobot <return.of.octobot@gmail.com>
2017-03-08 00:09:37 -0500

    drivers: media: msm: camera_v2: fix misleading-indentation warning drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c: In function 'msm_isp_get_done_buf': drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c:1198:4: warning: this 'for' clause does not guard... [-Wmisleading-indentation]
    Signed-off-by: Adam Willis <return.of.octobot@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1198,13 +1198,10 @@ static void msm_isp_get_done_buf(struct vfe_device *vfe_dev,
 			for (i = 0; i < VFE_AXI_SRC_MAX; i++)
 				vfe_dev->axi_data.stream_info[i].state =
 					INACTIVE;
+			error_event.frame_id =
+				vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id;
 
-				error_event.frame_id =
-					vfe_dev->axi_data.src_info[VFE_PIX_0].
-						frame_id;
-
-				msm_isp_send_event(vfe_dev,
-					ISP_EVENT_IOMMU_P_FAULT, &error_event);
+			msm_isp_send_event(vfe_dev, ISP_EVENT_IOMMU_P_FAULT, &error_event);
 
 			}
 		stream_info->sw_ping_pong_bit ^= 1;
